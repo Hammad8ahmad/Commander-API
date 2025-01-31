6225760308658f47811d2939124e1218f285159a
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-01-31 14:38:17 +0000

    Initial commit

---
+++ src/main/java/com/hammad/Command_snippet_API/CommandSnippetApiApplication.java
@@ -0,0 +1,13 @@
+package com.hammad.Command_snippet_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandSnippetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandSnippetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hammad/Command_snippet_API/CommandSnippetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammad.Command_snippet_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandSnippetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
