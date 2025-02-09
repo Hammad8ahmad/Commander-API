4bc524a103fcec84c9afb530735268c1a254e632
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-02-09 13:30:15 +0000

    configured some workflow for dev and prod env

--- src/main/java/com/hammad/Command_snippet_API/CommandSnippetApiApplication.java
+++ src/main/java/com/hammad/Command_snippet_API/CommandSnippetApiApplication.java
@@ -1,14 +1,14 @@
 package com.hammad.Command_snippet_API;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.annotations.RouterOperation;
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.info.Info;
+//import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @SpringBootApplication
@@ -17,10 +17,10 @@ public class CommandSnippetApiApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure()
-				.directory("./")  // Make sure it's pointing to the directory where your .env files are located
-				.filename(".env." + System.getProperty("spring.profiles.active"))
-				.load();
+//		Dotenv dotenv = Dotenv.configure()
+//				.directory("./")
+//				.filename(".env.prod")
+//				.load();
 
 
 
