bd9755296eecd999627805c9010b34f68b077fff
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-03-13 13:18:47 +0000

    testing caching

--- src/main/java/com/hammad/Command_snippet_API/CommandSnippetApiApplication.java
+++ src/main/java/com/hammad/Command_snippet_API/CommandSnippetApiApplication.java
@@ -1,14 +1,9 @@
 package com.hammad.Command_snippet_API;
 
-import io.github.cdimascio.dotenv.Dotenv;
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.info.Info;
-//import org.springdoc.core.annotations.RouterOperation;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @SpringBootApplication
@@ -16,11 +11,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class CommandSnippetApiApplication {
 
 	public static void main(String[] args) {
-
-//		Dotenv dotenv = Dotenv.configure()
-//				.directory("./")
-//				.filename(".env.prod")
-//				.load();
 
 
 
