032052d5050ce69fa5566d4804de968250282176
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-04-23 12:53:21 +0100

    added cors

--- src/main/java/com/hammad/Command_snippet_API/Config/SwaggerConfig.java
+++ src/main/java/com/hammad/Command_snippet_API/Config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.hammad.Command_snippet_API.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
+
+import java.util.List;
 
 
 // Swagger config file where you can configure different properties
@@ -14,8 +17,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://commander-api.duckdns.org")))
                 .info(new Info().title("Commander API")
                         .version("1.0")
                         .description("API documentation for managing command-line snippets."));
+
     }
 }
