76e8771e699c61ed84975e65ddba9e9884867f11
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-04-23 11:45:57 +0100

    added cors

---
+++ src/main/java/com/hammad/Command_snippet_API/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hammad.Command_snippet_API.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Or specify "http://localhost:3000"
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
